5ecdcdc8da84f0848c2defd8d5a5b96aaf6af2ac
Sangam <girisangam00@gmail.com>
2022-07-15 16:23:18 +0545

    textformfield_2

--- lib/README.txt
+++ lib/README.txt
@@ -1,3 +1,8 @@
 /*In this project we will be learning about the TextFormFile in Flutter*/
 
 This is somewhat similar to the Text Field but with a little more properties and features
+TextForm field is suitable for forms and provides extra features like validation
+
+One of the Major difference between TextField and TextFormField is TextFiled uses controller to validate whereas the TextFormFiled uses Validator
+
+For simple text inputs use TextField and for the complex ones go with the TextFormField
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -9,9 +9,96 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  bool _secureText = true;
+  TextEditingController _passwordController = TextEditingController();
+  var _passwordError;
+
+  var _formKey = GlobalKey<FormState>();
+
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+
+
+          /*TextField for the Password Field*/
+          TextField(
+          controller: _passwordController,
+            decoration: InputDecoration(
+              hintText: "Password",
+            labelText: "Password",
+            border: OutlineInputBorder(),
+
+            errorText: _passwordError, 
+            
+
+
+            suffixIcon: IconButton(onPressed: (){
+              setState(() {
+                _secureText = !_secureText;
+              });
+            },
+            icon: Icon(_secureText ? Icons.remove_red_eye: Icons.remove_red_eye_outlined),
+            ),
+
+            ),
+
+            obscureText: _secureText,
+          ),
+
+
+
+          /*TextForm Filed*/
+          Form(
+          key: _formKey,
+          child: TextFormField(
+           validator: (value) {
+            //  if(value!.isEmpty || RegExp(r'^[a-z A-Z 0-9]+$').hasMatch(value)){
+             if(value!.isEmpty || RegExp(r'^[0-9]+$').hasMatch(value)){
+               return "Enter valid password";
+             }else{
+               return null;
+             }
+             
+           },
+            decoration: InputDecoration(
+              hintText: "Password",
+              labelText: "Password",
+              labelStyle: TextStyle(color: Colors.grey),
+              border: OutlineInputBorder(),
+            ),
+          ),
+          
+        ),
+
+
+
+
+        /*Elevated Button*/
+
+          ElevatedButton(onPressed: (){
+            print(_passwordController.text);
+          //This has to be done inside setState then only the UI will get refreshed
+          setState(() {
+            _formKey.currentState!.validate();
+               if(_passwordController.text.length < 8 ){
+              _passwordError = "Enter atleast 8 characters";
+            }
+            else{
+              _passwordError = null;
+            }
+          });
+
+          },
+          child: Text("Click here"),),
+        ],
+        mainAxisAlignment: MainAxisAlignment.center,
+      ),
+    );
     
   }
-}
+} 
+    
